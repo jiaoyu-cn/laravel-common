304806c629a02c2862c3b2d68369691e8c7b00fd
githen <githen@foxmail.com>
2023-05-17 17:44:50 +0800

    添加功能类

--- src/app/Controllers/LogController.php
+++ src/app/Controllers/LogController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * 日志查看
@@ -24,7 +25,11 @@ class LogController extends  Controller
         return $this->$act($request);
     }
 
-
+    /**
+     * 日志列表
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\View
+     */
     private function index(Request $request)
     {
         $logFiles = File::allFiles(storage_path('logs/'));
@@ -35,11 +40,139 @@ class LogController extends  Controller
 
         $curRoute = Route::current();
 
-        dd($curRoute->uri(333));
-
         return view()->file(__DIR__.'/../../resources/views/log.blade.php', [
             'files' => $logFiles,
             'uri' => $curRoute->uri(),
         ]);
+    }
+
+    /**
+     * 日志文件处理
+     * @param Request $request
+     * @return void
+     */
+    private function download(Request $request)
+    {
+        // 检测文件是否存在
+        $file = 'logs/'.$request->input('file', 'xxx***');
+        if (! Storage::exists($file)){
+            return response('文件不存在', 404);
+        }
+
+        // 删除日志
+        if ($request->input('remove') == 'true'){
+            if(! Storage::delete($file)){
+                return response('文件删除失败', 404);
+            }
+            return redirect()->route(Route::current()->getName(), ['act' => 'index']);
+        }
+
+        // 下载日志
+        if ($request->input('download') == 'true'){
+            return response()->download(Storage::path($file));
+        }
+
+        // 查看日志
+        echo '<pre>';
+        $handle = fopen(Storage::path($file), 'r');
+        while (feof($handle) === false) {
+            echo fgets($handle);
+        }
+        fclose($handle);
+    }
+
+    /**
+     * 查看进程
+     * @param Request $request
+     * @return string
+     */
+    private function ps(Request $request)
+    {
+        $key = $request->input('key', 'laravel');
+        if (empty($key)){
+            return response("输入错误", 500);
+        }
+
+        $cmd = 'ps aux | grep -v grep | grep -e STARTED -e ' . $key;
+        exec($cmd, $out);
+        array_unshift($out, '执行命令：'.$cmd."<br>");
+
+        return '<pre>'.implode('<br>', $out);
+    }
+
+    /**
+     * 查看目录
+     * @param Request $request
+     * @return string
+     */
+    private function ls(Request $request)
+    {
+        $key = $request->input('key', '');
+        $cmd = 'ls -alh ' . storage_path($key);
+        exec($cmd, $out);
+        array_unshift($out, '执行命令：'.$cmd."<br>");
+
+        return '<pre>'.implode('<br>', $out);
+    }
+
+    /**
+     * 修改文件/目录权限
+     * @param Request $request
+     * @return string
+     */
+    private function chown(Request $request)
+    {
+        $key = $request->input('key', '');
+        $dir = base_path($key);
+
+        if (!is_file($dir) && !is_dir($dir)){
+            return response('文件/目录不存在', 500);
+        }
+
+        $cmd = 'chown -R 0770 ' . $dir;
+        exec($cmd, $out);
+        array_unshift($out, '执行命令：'.$cmd."<br>");
+
+        return '<pre>'.implode('<br>', $out);
+    }
+
+    /**
+     * 清理日志文件
+     * @param Request $request
+     * @return string
+     */
+    private function clean(Request $request)
+    {
+        $key = (int)$request->input('key', 7);
+
+        if ($key <= 3){
+            return response('日志保留天数最小为3天', 500);
+        }
+
+        // 清理文件及目录
+        $cmd = 'find '.storage_path('logs') .' -type f -ctime +4 | grep -e "\.log" -e "\.sql"';
+        exec($cmd, $out);
+
+        if (count($out) == 0){
+            return redirect()->route(Route::current()->getName(), ['act' => 'index']);
+        }
+
+        // 执行文件
+        foreach ($out as $file){
+            @unlink($file);
+        }
+
+        array_unshift($out, '执行命令：'.$cmd."<br>", '删除文件如下：<br>');
+
+        return '<pre>'.implode('<br>', $out);
+    }
+
+    /**
+     * 查看phpinfo
+     * @return void
+     */
+    public function phpinfo()
+    {
+        phpinfo();
     }
 }
--- src/resources/views/log.blade.php
+++ src/resources/views/log.blade.php
@@ -4,10 +4,32 @@
     <script src="https://www.eol.cn/e_js/index/2022/jquery.min.js" ignoreapd="false"></script>
 
     <style>
+        a {
+            text-decoration: none;
+        }
+        body {
+            width: 1024px;
+        }
+        .menu a {
+            border: #0b0d0f 1px solid;
+            padding: 1px;
+        }
+        .body{
+            clear: both;
+            overflow: hidden;
+        }
+        .left{
+            float: left;
+        }
+        .right {
+            float: right;
+        }
         dt{
             background-color: #ffb07c;
             margin: 5px;
             padding: 5px;
+            width: 1008px;
+            overflow: hidden;
         }
         dl dd{
             height: 30px;
@@ -29,30 +51,46 @@
     </style>
 </head>
 <body>
-<div>
-    <a href="javascript:;" onclick="logShow(true)">全部展开</a>
-    <a href="javascript:;" onclick="logShow(false)">全部隐藏</a>
+<div class="menu">
+    <div class="left">
+        <a target="_blank" href="/{{str_replace('{act}', 'ps',$uri)}}?key=laravel">查看进程</a>
+        <a target="_blank" href="/{{str_replace('{act}', 'ls',$uri)}}?key=">查看目录</a>
+        <a target="_blank" href="/{{str_replace('{act}', 'chown',$uri)}}?key=">修改权限</a>
+        <a target="_blank" href="/{{str_replace('{act}', 'clean',$uri)}}?key=7">清理目录</a>
+        <a target="_blank" href="/{{str_replace('{act}', 'phpinfo',$uri)}}">phpinfo</a>
+    </div>
+
+    <div class="right">
+        <a href="javascript:;" onclick="logShow(true)">全部展开</a>
+        <a href="javascript:;" onclick="logShow(false)">全部隐藏</a>
+    </div>
+
 </div>
+<div class="body">
 @foreach($files as $dir => $items)
     <dl>
-        <dt onclick="logToggle()">目录：<strong>{{$dir?:'.'}}</strong>  共计: {{count($items)}} 个</dt>
-        @foreach($items as $item )
+        <dt onclick="logToggle()">
+            <div class="left">目录：<strong>{{$dir?:'.'}}</strong> </div>
+            <div class="right"> {{count($items)}} 个 </div>
+        </dt>
+        @foreach($items->sortByDesc(function ($v,$k){return $v->getRelativePathname();}) as $item )
             <dd>
                 <div class="name">{{$item->getFilename()}}</div>
                 <div class="option">
-                    <a href="javascript:;">查看</a>
-                    <a href="javascript:;">下载</a>
-                    <a href="javascript:;">删除</a>
+                    <a target="_blank" href="/{{str_replace('{act}', 'download',$uri)}}?file={{$item->getRelativePathname()}}">查看</a>
+                    <a target="_blank" href="/{{str_replace('{act}', 'download',$uri)}}?file={{$item->getRelativePathname()}}&download=true">下载</a>
+                    <a href="/{{str_replace('{act}', 'download',$uri)}}?file={{$item->getRelativePathname()}}&remove=true">删除</a>
                 </div>
 
             </dd>
         @endforeach
     </dl>
 @endforeach
+</div>
 
 <script>
+    // 所有节点展示、隐藏处理
     function logShow(isShow) {
-
         if(isShow){
             $('dd').show();
         }else{
@@ -60,8 +98,8 @@
         }
     }
 
+    // dt中节点处理
     $('dt').on('click', function (){
-
         $(this).closest('dl').find('dd').toggle('slow');
     });
 
