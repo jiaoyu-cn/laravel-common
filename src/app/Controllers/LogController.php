0094d0d8c078e5902e33a90bd737758fc15f5e76
githen <githen@foxmail.com>
2023-05-17 11:33:29 +0800

    添加controller

--- src/app/Controllers/LogController.php
+++ src/app/Controllers/LogController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace Githen\App\Controllers;
+namespace Githen\LaravelCommon\App\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
 
 /**
  * 日志查看
@@ -25,7 +27,19 @@ class LogController extends  Controller
 
     private function index(Request $request)
     {
-        dd(__CLASS__, __FUNCTION__);
+        $logFiles = File::allFiles(storage_path('logs/'));
 
+        $logFiles = collect($logFiles)->mapToGroups(function ($item, $key){
+            return [$item->getRelativePath() => $item];
+        })->sortKeys();
+
+        $curRoute = Route::current();
+
+        dd($curRoute->uri(333));
+
+        return view()->file(__DIR__.'/../../resources/views/log.blade.php', [
+            'files' => $logFiles,
+            'uri' => $curRoute->uri(),
+        ]);
     }
 }
--- src/resources/views/log.blade.php
+++ src/resources/views/log.blade.php
@@ -1,1 +1,78 @@
-<?php
+<html>
+<head>
+    <title>查看日志</title>
+    <script src="https://www.eol.cn/e_js/index/2022/jquery.min.js" ignoreapd="false"></script>
+
+    <style>
+        dt{
+            background-color: #ffb07c;
+            margin: 5px;
+            padding: 5px;
+        }
+        dl dd{
+            height: 30px;
+            line-height: 30px;
+            width: 100%;
+        }
+
+        dl dd:nth-child(even){
+            background-color: #e3f2fd;
+        }
+        .name {
+            font-weight: bold;
+            display: inline-block;
+        }
+        .option {
+            display: inline-block;
+        }
+
+    </style>
+</head>
+<body>
+<div>
+    <a href="javascript:;" onclick="logShow(true)">全部展开</a>
+    <a href="javascript:;" onclick="logShow(false)">全部隐藏</a>
+</div>
+@foreach($files as $dir => $items)
+    <dl>
+        <dt onclick="logToggle()">目录：<strong>{{$dir?:'.'}}</strong>  共计: {{count($items)}} 个</dt>
+        @foreach($items as $item )
+            <dd>
+                <div class="name">{{$item->getFilename()}}</div>
+                <div class="option">
+                    <a href="javascript:;">查看</a>
+                    <a href="javascript:;">下载</a>
+                    <a href="javascript:;">删除</a>
+                </div>
+
+            </dd>
+        @endforeach
+    </dl>
+@endforeach
+
+<script>
+    function logShow(isShow) {
+
+        if(isShow){
+            $('dd').show();
+        }else{
+            $('dd').hide();
+        }
+    }
+
+    $('dt').on('click', function (){
+
+        $(this).closest('dl').find('dd').toggle('slow');
+    });
+
+    // 修改长度
+    $('dl').each(function (i){
+        let tmpWidth = 0;
+        $(this).find('dd div.name').each(function (j) {
+            tmpWidth = Math.max($(this).outerWidth(true), tmpWidth);
+        });
+        $(this).find('dd div.name').width((tmpWidth + 20) + 'px');
+    });
+</script>
+</body>
+</html>
