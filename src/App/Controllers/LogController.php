d21e9454f9ffb8f63186659b240f24ccfcd5f08b
nanjishidu <yijialiu212@gmail.com>
2026-05-29 11:30:43 +0800

    edit controller

--- src/App/Controllers/LogController.php
+++ src/App/Controllers/LogController.php
@@ -2,7 +2,7 @@
 
 namespace Githen\LaravelCommon\App\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
